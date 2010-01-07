ea8d3f5eddf06e599eea0f7f5cf70152e63f505d
manuel.kirschner <manuel.kirschner@5a2507a2-fb87-11de-813e-e90011bb59b3>
2010-01-07 14:14:46 +0000

    initial

---
+++ trunk/Macroparser/src/it/unibz/lib/macroparser/macroParser_TreeParser.java
@@ -0,0 +1,69 @@
+// $ANTLR : "macroParser.g" -> "macroParser_TreeParser.java"$
+
+package it.unibz.lib.macroparser;
+
+import antlr.TreeParser;
+import antlr.Token;
+import antlr.collections.AST;
+import antlr.RecognitionException;
+import antlr.ANTLRException;
+import antlr.NoViableAltException;
+import antlr.MismatchedTokenException;
+import antlr.SemanticException;
+import antlr.collections.impl.BitSet;
+import antlr.ASTPair;
+import antlr.collections.impl.ASTArray;
+
+
+public class macroParser_TreeParser extends antlr.TreeParser       implements macroParser_LexerTokenTypes
+ {
+public macroParser_TreeParser() {
+	tokenNames = _tokenNames;
+}
+
+	public final String  string(AST _t) throws RecognitionException {
+		String result;
+		
+		AST string_AST_in = (_t == ASTNULL) ? null : (AST)_t;
+		AST ast = null;
+		
+			result="";
+		
+		
+		try {      // for error handling
+			ast = (AST)_t;
+			match(_t,STRING_);
+			_t = _t.getNextSibling();
+			
+					 	AST firstchild = ast.getFirstChild(); 
+					 	do {
+					 		if (firstchild.getType() == MACRO) {
+					 			result += "MACRO";
+					 		} else if (firstchild.getType() == NON_MACRO) {
+					 			result += firstchild.getFirstChild().toString();
+					 		}
+					 	} while 
+							((firstchild = firstchild.getNextSibling()) != null);
+					
+		}
+		catch (RecognitionException ex) {
+			reportError(ex);
+			if (_t!=null) {_t = _t.getNextSibling();}
+		}
+		_retTree = _t;
+		return result;
+	}
+	
+	
+	public static final String[] _tokenNames = {
+		"<0>",
+		"EOF",
+		"<2>",
+		"NULL_TREE_LOOKAHEAD",
+		"NON_MACRO",
+		"MACRO",
+		"STRING_"
+	};
+	
+	}
+	
